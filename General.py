f59bfca715be3a2f2e076bc498f4978f1b3d7eb5
bogdan-nikitin <nikitin.bogdan333@yandex.ru>
2019-12-29 21:45:20 +0300

    Обновил General, добавив функцию cut_sheet и комментарии.

--- General.py
+++ General.py
@@ -1,8 +1,12 @@
-import os
 from Constants import *
+import os
+import pygame
 
 
 def load_image(name, color_key=None):
+    """Загружает изображение data/name и устанавливает изображению цветовой ключ
+     color_key. Если color_key=-1, то в качестве color_key используется верхний
+     левый угол зображения."""
     fullname = os.path.join('data', name)
     image = pygame.image.load(fullname)
     if color_key is not None:
@@ -16,8 +20,23 @@ def load_image(name, color_key=None):
 
 
 def clear(screen):
+    """Очищает экран screen, заливая его цветом Constants.BG_COLOR."""
     screen.fill(BG_COLOR)
 
 
 def is_pressed(key):
+    """Возвращает True или False, в зависимости от того, нажата ли клавиша key.
+    Номера клавиш используются из библиотеки pygame"""
     return bool(pygame.key.get_pressed()[key])
+
+
+def cut_sheet(sheet, columns, rows):
+    frames = []
+    rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                       sheet.get_height() // rows)
+    for j in range(rows):
+        for i in range(columns):
+            frame_location = (rect.w * i, rect.h * j)
+            frames.append(sheet.subsurface(pygame.Rect(
+                frame_location, rect.size)))
+    return frames
